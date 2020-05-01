27ea99270a02dc2818c6582f12a192ab00fe39b6
oppilate <oppilate@tutanota.com>
2020-05-01 17:02:26 +0800

    Add site list for rsshub and rssbridge to crawl

---
+++ src/sites/rssbridgeSites.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const rssbridgeParameters = [
+  '&bridge=TheGuardian&feed=%2Fus-news',
+
+  '&bridge=TheGuardian&feed=%2Fuk-news',
+
+  '&bridge=TheGuardian&feed=%2Feurope-news',
+  '&bridge=TheGuardian&feed=%2Fasia-news',
+  '&bridge=TheGuardian&feed=%2Fbusiness-news',
+];
